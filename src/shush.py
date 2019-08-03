578b3e5cac094649e95f2a68edb158bbef28f093
Plech12 <plechms@gmail.com>
2019-08-03 13:51:20 +0200

    First few lines of code

--- src/shush.py
+++ src/shush.py
@@ -1,1 +1,16 @@
-print("i like trains")
+print("Let's code some messages!")
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+
+Keynumber = int(input("Enter the key :"))
+
+newMessage = ''
+
+message1 = input('Enter the message :')
+
+for letter in message1:
+    position = alphabet.find(letter)
+    newPosition = (position + Keynumber)
+    newCharacter = alphabet[newPosition]
+    print('The message is :', newCharacter)
